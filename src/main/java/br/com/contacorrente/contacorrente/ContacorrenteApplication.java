aad2c511f5dff224367b21d71427121bcbf374a3
Rafael <rafael.alves.1@hotmail.com>
2022-01-20 23:19:27 -0300

    alterando estruturas do projeto

---
+++ src/main/java/br/com/contacorrente/contacorrente/ContacorrenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.contacorrente.contacorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContacorrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContacorrenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/contacorrente/contacorrente/ContacorrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contacorrente.contacorrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContacorrenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
